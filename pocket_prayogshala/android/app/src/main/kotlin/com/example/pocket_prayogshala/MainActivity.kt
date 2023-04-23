9fdf22eaa2d9989349ad9a0519ca06bfeac91e8f
Mukesh Tiwari <mukeshtiwari200102@gmail.com>
2023-04-23 05:53:10 +0545

    new brnach for publishing

--- pocket_prayogshala/android/app/src/main/kotlin/com/example/pocket_prayogshala/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pocket_prayogshala
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- pocket_prayogshala/lib/mycolors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color appBarBg = Color.fromARGB(255, 255, 255, 255);
-//Color appBarText = Colors.amber.shade400;
-Color appBarShadow = Color.fromARGB(255, 188, 162, 83);
-Color mainBg = Colors.amber.shade400;
